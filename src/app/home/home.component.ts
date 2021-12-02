50b165f3c2fb95a32700132c060db0ff96f6c1be
Angela <angela.stevkovska3@gmail.com>
2021-12-02 13:03:54 +0100

    home page banners

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,12 +11,14 @@ export class HomeComponent implements OnInit {
   constructor(private productService:ProductService) { }
   isDataLoaded:boolean=false;
   productList:any;
-
+  reverseList:any;
   ngOnInit(): void {
     this.productService.getProducts().then(result => {
       this.productList = result;
       this.isDataLoaded=true;
+      this.reverseList = this.productList.reverseList.slice(this.productList.length-3)
     })
   }
+
 
 }
