eb47d0fc109849b1455df8cfcba48524dfc985f6
Filip Josifoski <Filip.Josifoski@ats-global.com>
2021-12-03 09:30:37 +0100

    Add input for image select

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
-import {ProductService} from "../services/product.service";
-import {Product} from "../models/product";
-import {UsersService} from "../services/users.service";
-import {AuthenticationService} from "../services/authentication.service";
-import {ActivatedRoute, Router} from "@angular/router";
+import { FormControl, FormGroup } from "@angular/forms";
+import { ProductService } from "../services/product.service";
+import { Product } from "../models/product";
+import { UsersService } from "../services/users.service";
+import { AuthenticationService } from "../services/authentication.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 
 @Component({
@@ -15,22 +15,22 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class AddProductComponent implements OnInit {
 
 
-  genders=[
-    { name:'Male'},
-    { name:'Female'},
-    { name:'Kids'},
+  genders = [
+    { name: 'Male' },
+    { name: 'Female' },
+    { name: 'Kids' },
   ];
 
-  categories=[
-    { name:'T-shirts'},
-    { name:'Sweaters'},
-    { name:'Skirts'},
-    { name:'Accessories'},
-    { name:'Jeans'},
-    { name:'Accessories'},
-    { name:'Coats'},
-    { name:'Scarfs'},
-    { name:'Bags'},
+  categories = [
+    { name: 'T-shirts' },
+    { name: 'Sweaters' },
+    { name: 'Skirts' },
+    { name: 'Accessories' },
+    { name: 'Jeans' },
+    { name: 'Accessories' },
+    { name: 'Coats' },
+    { name: 'Scarfs' },
+    { name: 'Bags' },
   ]
   addProductForm = new FormGroup({
     productName: new FormControl(''),
@@ -42,23 +42,42 @@ export class AddProductComponent implements OnInit {
     image: new FormControl('')
 
   });
-  constructor(private productService:ProductService,private userService:UsersService,
-              private authService:AuthenticationService,private router: Router) {
+  selectedFile: any;
+  constructor(private productService: ProductService, private userService: UsersService,
+    private authService: AuthenticationService, private router: Router) {
+      document.title="Add product"
   }
 
   ngOnInit(): void {
   }
 
-  email:any;
-  onSubmit(){
-    const {image, productName,categoryGender,categoryType, price, description, size}=this.addProductForm.value;
-    console.log("product",this.addProductForm.value);
-    this.email=this.authService.currentUserValue?.email;
-    this.productService.addProduct(new Product(image, productName, [categoryGender.name,categoryType.name], price,
-      description, this.email,size,new Date()));
+  email: any;
+  onSubmit() {
+    const { image, productName, categoryGender, categoryType, price, description, size } = this.addProductForm.value;
+    console.log("product", this.addProductForm.value);
+    this.email = this.authService.currentUserValue?.email;
+    this.productService.addProduct(new Product(image, productName, [categoryGender.name, categoryType.name], price,
+      description, this.email, size, new Date()));
     console.log(this.productService.getProducts())
     this.addProductForm.reset();
     this.router.navigate(['/shop']);
   }
 
+  processImage(imageInput: any) {
+    const file: File = imageInput.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', (event: any) => {
+      this.selectedFile = new ImageSnippet(event.target.result, file);
+      this.addProductForm.controls['image'].setValue(this.selectedFile.src);
+    });
+
+    reader.readAsDataURL(file);
+  }
+
 }
+
+class ImageSnippet {
+  constructor(public src: string, public file: File) { }
+}
+
